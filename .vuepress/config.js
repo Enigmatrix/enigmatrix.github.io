1d03598281b75af791012d9ebafa596bc0d9de20
Enigmatrix <enigmatrix2000@gmail.com>
2019-06-10 22:17:22 +0800

    blog started

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  plugins: ["@silvanite/tailwind"]
+  plugins: {
+    "@vuepress/blog": {
+      directories: [
+        {
+          // Unique ID of current classification
+          id: 'post',
+          // Target directory
+          dirname: '_posts',
+          // Path of the `entry page` (or `list page`)
+          path: '/',
+        },
+      ],
+    },
+    "@silvanite/tailwind": {},
+  },
+  title: "Enigmatrix",
+  description: "Enigmatrix's mark on the Web"
 };
